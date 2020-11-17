011694c797c2c06ddec915acd60098c155ef8467
Raphael Traviss <raphael@skyleafdesign.com>
2020-11-17 12:32:20 -0600

    FastAPI serving user REST resource

--- src/server_x/main.py
+++ src/server_x/main.py
@@ -1,8 +1,57 @@
-from fastapi import FastAPI
+# pylint: disable=E0611
+from typing import List
 
-app = FastAPI()
+from fastapi import FastAPI, HTTPException
+from models import User_Pydantic, UserIn_Pydantic, Users
+from pydantic import BaseModel
+
+from tortoise.contrib.fastapi import HTTPNotFoundError, register_tortoise
+
+app = FastAPI(title="Tortoise ORM FastAPI example")
 
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
+class Status(BaseModel):
+    message: str
+
+
+@app.get("/users", response_model=List[User_Pydantic])
+async def get_users():
+    return await User_Pydantic.from_queryset(Users.all())
+
+
+@app.post("/users", response_model=User_Pydantic)
+async def create_user(user: UserIn_Pydantic):
+    user_obj = await Users.create(**user.dict(exclude_unset=True))
+    return await User_Pydantic.from_tortoise_orm(user_obj)
+
+
+@app.get(
+    "/user/{user_id}", response_model=User_Pydantic, responses={404: {"model": HTTPNotFoundError}}
+)
+async def get_user(user_id: int):
+    return await User_Pydantic.from_queryset_single(Users.get(id=user_id))
+
+
+@app.put(
+    "/user/{user_id}", response_model=User_Pydantic, responses={404: {"model": HTTPNotFoundError}}
+)
+async def update_user(user_id: int, user: UserIn_Pydantic):
+    await Users.filter(id=user_id).update(**user.dict(exclude_unset=True))
+    return await User_Pydantic.from_queryset_single(Users.get(id=user_id))
+
+
+@app.delete("/user/{user_id}", response_model=Status, responses={404: {"model": HTTPNotFoundError}})
+async def delete_user(user_id: int):
+    deleted_count = await Users.filter(id=user_id).delete()
+    if not deleted_count:
+        raise HTTPException(status_code=404, detail=f"User {user_id} not found")
+    return Status(message=f"Deleted user {user_id}")
+
+
+register_tortoise(
+    app,
+    db_url="sqlite://:memory:",
+    modules={"models": ["models"]},
+    generate_schemas=True,
+    add_exception_handlers=True,
+)
